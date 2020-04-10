4b96a1d22661d6b2a07a96bcd7aba2812be939c1
YH ONG <ongyh97@gmail.com>
2020-04-10 17:52:41 +0800

    refactor: rename the filename

--- src/FaceDetection/CountFaceDetection.java
+++ src/FaceDetection/CountFaceDetection.java
@@ -23,12 +23,12 @@ import org.opencv.objdetect.CascadeClassifier;
  *
  * @author YH.Ong
  */
-public class FaceDetection extends javax.swing.JFrame {
+public class CountFaceDetection extends javax.swing.JFrame {
 
     /**
      * Creates new form FaceDetection
      */
-    public FaceDetection() {
+    public CountFaceDetection() {
         initComponents();
     }
     
@@ -176,20 +176,21 @@ public class FaceDetection extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FaceDetection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CountFaceDetection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FaceDetection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CountFaceDetection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FaceDetection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CountFaceDetection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FaceDetection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CountFaceDetection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FaceDetection().setVisible(true);
+                new CountFaceDetection().setVisible(true);
             }
         });
     }
